bb80ab211006f2559eab4975345fbba9a6edff7a
benkuly <3-benkuly@users.noreply.gitlab.folivo.net>
2021-05-06 11:01:25 +0200

    do not publish examples

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,60 +28,63 @@ inline val Project.isRelease
     get() = !version.toString().contains('-')
 
 subprojects {
-    apply(plugin = "maven-publish")
-    apply(plugin = "signing")
-
     val dokkaJavadocJar by tasks.creating(Jar::class) {
         dependsOn(tasks.dokkaJavadoc)
         from(tasks.dokkaJavadoc.get().outputDirectory.get())
         archiveClassifier.set("javadoc")
     }
 
-    publishing {
-        publications.configureEach {
-            if (this is MavenPublication) {
-                pom {
-                    name.set(project.name)
-                    description.set("Multiplatform Kotlin SDK for matrix-protocol")
-                    url.set("https://gitlab.folivo.net/benkuly/trixnity")
-                    licenses {
-                        license {
-                            name.set("GNU Affero General Public License, Version 3.0")
-                            url.set("http://www.gnu.org/licenses/agpl-3.0.de.html")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("benkuly")
-                        }
-                    }
-                    scm {
+    if (!project.name.startsWith("trixnity-examples")) {
+        apply(plugin = "maven-publish")
+        apply(plugin = "signing")
+
+        publishing {
+            publications.configureEach {
+                if (this is MavenPublication) {
+                    pom {
+                        println("sub " + project.name)
+                        name.set(project.name)
+                        description.set("Multiplatform Kotlin SDK for matrix-protocol")
                         url.set("https://gitlab.folivo.net/benkuly/trixnity")
+                        licenses {
+                            license {
+                                name.set("GNU Affero General Public License, Version 3.0")
+                                url.set("http://www.gnu.org/licenses/agpl-3.0.de.html")
+                            }
+                        }
+                        developers {
+                            developer {
+                                id.set("benkuly")
+                            }
+                        }
+                        scm {
+                            url.set("https://gitlab.folivo.net/benkuly/trixnity")
+                        }
+
+                        artifact(dokkaJavadocJar)
                     }
-
-                    artifact(dokkaJavadocJar)
+                }
+            }
+            repositories {
+                maven {
+                    name = "OSSRH"
+                    url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    credentials {
+                        username = System.getenv("OSSRH_USERNAME")
+                        password = System.getenv("OSSRH_PASSWORD")
+                    }
                 }
             }
         }
-        repositories {
-            maven {
-                name = "OSSRH"
-                url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-                credentials {
-                    username = System.getenv("OSSRH_USERNAME")
-                    password = System.getenv("OSSRH_PASSWORD")
-                }
-            }
-        }
-    }
 
-    signing {
-        isRequired = isRelease
-        useInMemoryPgpKeys(
-            System.getenv("OSSRH_PGP_KEY"),
-            System.getenv("OSSRH_PGP_PASSWORD")
-        )
-        sign(publishing.publications)
+        signing {
+            isRequired = isRelease
+            useInMemoryPgpKeys(
+                System.getenv("OSSRH_PGP_KEY"),
+                System.getenv("OSSRH_PGP_PASSWORD")
+            )
+            sign(publishing.publications)
+        }
     }
 }
 
