94623aca037cbf67be54619add917c8987c214b1
Michael Thiele <thiele.michael2@googlemail.com>
2021-12-06 08:58:38 +0000

    fix the coroutine context in the example to be single-threaded in order to...

--- examples/client-multiplatform-ping/src/jvmMain/kotlin/net/folivo/trixnity/examples/multiplatform/createDriver.kt
+++ examples/client-multiplatform-ping/src/jvmMain/kotlin/net/folivo/trixnity/examples/multiplatform/createDriver.kt
@@ -2,7 +2,6 @@ package net.folivo.trixnity.examples.multiplatform
 
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.newSingleThreadContext
 import kotlin.coroutines.CoroutineContext
@@ -11,8 +10,9 @@ actual fun createDriver(): SqlDriver {
     return JdbcSqliteDriver("jdbc:sqlite:test.db")
 }
 
+@OptIn(ExperimentalCoroutinesApi::class)
 actual fun databaseCoroutineContext(): CoroutineContext {
-    return Dispatchers.IO
+    return newSingleThreadContext("db")
 }
 
 @OptIn(ExperimentalCoroutinesApi::class)
