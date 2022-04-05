8be24d878a0dcad6db6f9a648a8b681e0e9e5160
benkuly <3976820-benkuly@users.noreply.gitlab.com>
2022-04-05 13:31:27 +0200

    moved PusherData.kt into package level

--- trixnity-clientserverapi/trixnity-clientserverapi-model/src/commonMain/kotlin/net/folivo/trixnity/clientserverapi/model/push/SetPushers.kt
+++ trixnity-clientserverapi/trixnity-clientserverapi-model/src/commonMain/kotlin/net/folivo/trixnity/clientserverapi/model/push/SetPushers.kt
@@ -1,15 +1,8 @@
 package net.folivo.trixnity.clientserverapi.model.push
 
 import io.ktor.resources.*
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.SerializationException
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.descriptors.buildClassSerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.json.*
 import net.folivo.trixnity.core.HttpMethod
 import net.folivo.trixnity.core.HttpMethodType.POST
 import net.folivo.trixnity.core.MatrixEndpoint
@@ -41,41 +34,5 @@ data class SetPushers(
         val profileTag: String? = null,
         @SerialName("pushkey")
         val pushkey: String,
-    ) {
-        @Serializable(with = PusherDataSerializer::class)
-        data class PusherData(
-            val format: String? = null,
-            val url: String? = null,
-            val customFields: JsonObject? = null
-        )
-    }
-}
-
-object PusherDataSerializer : KSerializer<SetPushers.Request.PusherData> {
-    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("PusherDataSerializer")
-
-    override fun deserialize(decoder: Decoder): SetPushers.Request.PusherData {
-        require(decoder is JsonDecoder)
-        val jsonObject = decoder.decodeJsonElement()
-        if (jsonObject !is JsonObject) throw SerializationException("data must be a JsonObject")
-        return SetPushers.Request.PusherData(
-            format = jsonObject["format"]?.let { decoder.json.decodeFromJsonElement(it) },
-            url = jsonObject["url"]?.let { decoder.json.decodeFromJsonElement(it) },
-            customFields = JsonObject(buildMap {
-                putAll(jsonObject)
-                remove("format")
-                remove("url")
-            })
-        )
-    }
-
-    override fun serialize(encoder: Encoder, value: SetPushers.Request.PusherData) {
-        require(encoder is JsonEncoder)
-        encoder.encodeJsonElement(JsonObject(buildMap {
-            value.format?.let { put("format", JsonPrimitive(it)) }
-            value.url?.let { put("url", JsonPrimitive(it)) }
-            value.customFields?.let { putAll(it) }
-        }))
-    }
-
+    )
 }
