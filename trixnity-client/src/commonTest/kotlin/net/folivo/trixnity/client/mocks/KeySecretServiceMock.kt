5a2e344ca3819346c31853a182bbe4068b6d1e40
benkuly <3976820-benkuly@users.noreply.gitlab.com>
2022-04-07 15:39:59 +0200

    remove mockk from SelfVerificationMethodTest.kt

--- trixnity-client/src/commonTest/kotlin/net/folivo/trixnity/client/mocks/KeySecretServiceMock.kt
+++ trixnity-client/src/commonTest/kotlin/net/folivo/trixnity/client/mocks/KeySecretServiceMock.kt
@@ -1,10 +1,12 @@
 package net.folivo.trixnity.client.mocks
 
+import kotlinx.coroutines.flow.MutableStateFlow
 import net.folivo.trixnity.client.key.IKeySecretService
 import net.folivo.trixnity.core.model.events.m.secretstorage.SecretKeyEventContent
 
 class KeySecretServiceMock : IKeySecretService {
+    val decryptMissingSecretsCalled = MutableStateFlow<Triple<ByteArray, String, SecretKeyEventContent>?>(null)
     override suspend fun decryptMissingSecrets(key: ByteArray, keyId: String, keyInfo: SecretKeyEventContent) {
-        throw NotImplementedError()
+        decryptMissingSecretsCalled.value = Triple(key, keyId, keyInfo)
     }
 }
