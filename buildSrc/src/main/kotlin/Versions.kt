acbfa998c80c0a5a300602911a99d33b95a030ba
benkuly <3976820-benkuly@users.noreply.gitlab.com>
2022-04-08 17:21:17 +0200

    revert version change of kotlinx coroutines

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     const val kotlin = "1.6.20" // https://github.com/JetBrains/kotlin/releases
     val kotlinJvmTarget = JavaVersion.VERSION_11
     const val ksp = "1.6.20-1.0.5" // https://github.com/google/ksp/releases
-    const val kotlinxCoroutines = "1.6.1" // https://github.com/Kotlin/kotlinx.coroutines/releases
+    const val kotlinxCoroutines = "1.6.0" // https://github.com/Kotlin/kotlinx.coroutines/releases
     const val kotlinxSerialization = "1.3.2" // https://github.com/Kotlin/kotlinx.serialization/releases
     const val kotlinxDatetime = "0.3.2" // https://github.com/Kotlin/kotlinx-datetime/releases
     const val kotlinxKover = "0.5.0" // https://github.com/Kotlin/kotlinx-kover/releases
