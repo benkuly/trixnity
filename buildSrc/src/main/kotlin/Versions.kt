5e90acf378144c9db9c494c066510ecbc548decb
benkuly <3976820-benkuly@users.noreply.gitlab.com>
2022-04-29 12:58:17 +0200

    update ktor

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -11,7 +11,7 @@ object Versions {
     const val dokka = "1.6.20" // https://github.com/Kotlin/dokka/releases
     const val olm = "3.2.8" // https://gitlab.matrix.org/matrix-org/olm/-/releases
     const val jna = "5.11.0" // https://github.com/java-native-access/jna/tags
-    const val ktor = "2.0.0" // https://github.com/ktorio/ktor/releases
+    const val ktor = "2.0.1" // https://github.com/ktorio/ktor/releases
     const val korlibs = "2.7.0" // https://github.com/korlibs/korge-next/releases
     const val mockative = "1.1.4" // https://github.com/mockative/mockative/releases
     const val uuid = "0.4.0" // https://github.com/benasher44/uuid/releases
