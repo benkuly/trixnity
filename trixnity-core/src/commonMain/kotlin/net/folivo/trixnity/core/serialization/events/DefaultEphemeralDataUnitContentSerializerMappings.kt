9c0aad0c7f43a55f623701ea8bd3ca5fbbc28232
benkuly <3976820-benkuly@users.noreply.gitlab.com>
2022-05-21 12:00:03 +0200

    add EDU typing and receipt

--- trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/serialization/events/DefaultEphemeralDataUnitContentSerializerMappings.kt
+++ trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/serialization/events/DefaultEphemeralDataUnitContentSerializerMappings.kt
@@ -1,8 +1,12 @@
 package net.folivo.trixnity.core.serialization.events
 
 import net.folivo.trixnity.core.model.events.m.PresenceDataUnitContent
+import net.folivo.trixnity.core.model.events.m.ReceiptDataUnitContent
+import net.folivo.trixnity.core.model.events.m.TypingDataUnitContent
 import net.folivo.trixnity.core.serialization.events.SerializerMapping.Companion.of
 
 val DefaultEphemeralDataUnitContentSerializerMappings: EphemeralDataUnitContentMappings = setOf(
-    of<PresenceDataUnitContent>("m.presence")
+    of<PresenceDataUnitContent>("m.presence"),
+    of<TypingDataUnitContent>("m.typing"),
+    of<ReceiptDataUnitContent>("m.receipt")
 )
