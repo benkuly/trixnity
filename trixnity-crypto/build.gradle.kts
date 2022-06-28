55a48f2f53c0c55031e2820b38f0ed1ace82cb46
benkuly <3976820-benkuly@users.noreply.gitlab.com>
2022-06-28 08:09:45 +0200

    move modules, create trixnity-crypto module

---
+++ trixnity-crypto/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+}
+
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(Versions.kotlinJvmTarget.majorVersion))
+    }
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = Versions.kotlinJvmTarget.toString()
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+        withJava()
+    }
+    js(IR) {
+        browser {
+            testTask {
+                useKarma {
+                    useFirefoxHeadless()
+                }
+            }
+        }
+        nodejs()
+        binaries.executable()
+    }
+
+    linuxX64()
+    mingwX64()
+    macosX64()
+    macosArm64()
+    ios()
+
+    targets.disableCompilationsOnCI()
+
+    sourceSets {
+        all {
+            languageSettings.optIn("kotlin.RequiresOptIn")
+        }
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.kotlinxCoroutines}")
+                implementation("io.github.microutils:kotlin-logging:${Versions.kotlinLogging}")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("io.kotest:kotest-assertions-core:${Versions.kotest}")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation("ch.qos.logback:logback-classic:${Versions.logback}")
+            }
+        }
+    }
+}
