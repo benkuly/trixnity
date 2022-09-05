88b33681bf1950ae2e6aaf331a94195aa8a9c12c
benkuly <3976820-benkuly@users.noreply.gitlab.com>
2022-09-05 11:00:09 +0200

    fix links and licence

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,11 +76,11 @@ subprojects {
                     pom {
                         name.set(project.name)
                         description.set("Multiplatform Kotlin SDK for matrix-protocol")
-                        url.set("https://gitlab.com/benkuly/trixnity")
+                        url.set("https://gitlab.com/trixnity/trixnity")
                         licenses {
                             license {
-                                name.set("GNU Affero General Public License, Version 3.0")
-                                url.set("http://www.gnu.org/licenses/agpl-3.0.de.html")
+                                name.set("Apache License 2.0")
+                                url.set("https://www.apache.org/licenses/LICENSE-2.0")
                             }
                         }
                         developers {
@@ -89,7 +89,7 @@ subprojects {
                             }
                         }
                         scm {
-                            url.set("https://gitlab.com/benkuly/trixnity")
+                            url.set("https://gitlab.com/trixnity/trixnity")
                         }
 
                         artifact(dokkaJavadocJar)
