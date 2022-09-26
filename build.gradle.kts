3200610816a97fda367d751e5ece48e765fe8d2f
benkuly <3976820-benkuly@users.noreply.gitlab.com>
2022-09-26 09:42:40 +0200

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 
 allprojects {
     group = "net.folivo"
-    version = "2.3.5" +
+    version = "2.3.6" +
             when {
                 isRelease -> ""
                 isCI -> "-SNAPSHOT"
