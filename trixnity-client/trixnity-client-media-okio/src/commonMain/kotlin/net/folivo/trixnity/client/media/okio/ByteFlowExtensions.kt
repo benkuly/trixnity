837415f062ce9adf0b689f70a3810b10db80aea8
benkuly <3976820-benkuly@users.noreply.gitlab.com>
2022-10-26 07:57:44 +0200

    change context of blocking okio operations

--- trixnity-client/trixnity-client-media-okio/src/commonMain/kotlin/net/folivo/trixnity/client/media/okio/ByteFlowExtensions.kt
+++ trixnity-client/trixnity-client-media-okio/src/commonMain/kotlin/net/folivo/trixnity/client/media/okio/ByteFlowExtensions.kt
@@ -2,20 +2,23 @@ package net.folivo.trixnity.client.media.okio
 
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import net.folivo.trixnity.core.ByteFlow
 import okio.*
 import kotlin.coroutines.CoroutineContext
 
-suspend fun ByteFlow.writeToSink(sink: BufferedSink): Unit =
-    collect { sink.writeByte(it.toInt()) }
+suspend fun ByteFlow.writeToSink(sink: BufferedSink, coroutineContext: CoroutineContext = defaultContext): Unit =
+    withContext(coroutineContext) {
+        collect { sink.writeByte(it.toInt()) }
+    }
 
-suspend fun BufferedSource.readByteFlow(): ByteFlow =
+suspend fun BufferedSource.readByteFlow(coroutineContext: CoroutineContext = defaultContext): ByteFlow =
     flow {
         while (exhausted().not()) {
             emit(readByte())
         }
-    }
+    }.flowOn(coroutineContext)
 
 suspend fun FileSystem.writeByteFlow(
     path: Path,
@@ -24,7 +27,7 @@ suspend fun FileSystem.writeByteFlow(
 ): Unit =
     withContext(coroutineContext) {
         sink(path).buffer().use { sink ->
-            content.writeToSink(sink)
+            content.writeToSink(sink, coroutineContext)
         }
     }
 
@@ -32,12 +35,10 @@ suspend fun FileSystem.readByteFlow(
     path: Path,
     coroutineContext: CoroutineContext = defaultContext,
 ): ByteFlow? =
-    withContext(coroutineContext) {
-        if (exists(path))
-            flow {
-                source(path).buffer().use { source ->
-                    emitAll(source.readByteFlow())
-                }
+    if (exists(path))
+        flow {
+            source(path).buffer().use { source ->
+                emitAll(source.readByteFlow(coroutineContext))
             }
-        else null
-    }
+        }.flowOn(coroutineContext)
+    else null
