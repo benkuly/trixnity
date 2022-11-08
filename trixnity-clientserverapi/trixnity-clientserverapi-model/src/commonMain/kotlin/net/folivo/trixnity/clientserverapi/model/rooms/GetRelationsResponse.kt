ffbe4125919de9ab5e68ca25e3cea36a712de618
benkuly <kontakt@folivo.net>
2022-11-08 16:00:19 +0100

    add GetThreads endpoint

--- trixnity-clientserverapi/trixnity-clientserverapi-model/src/commonMain/kotlin/net/folivo/trixnity/clientserverapi/model/rooms/GetRelationsResponse.kt
+++ trixnity-clientserverapi/trixnity-clientserverapi-model/src/commonMain/kotlin/net/folivo/trixnity/clientserverapi/model/rooms/GetRelationsResponse.kt
@@ -9,5 +9,5 @@ import net.folivo.trixnity.core.model.events.Event
 data class GetRelationsResponse(
     @SerialName("prev_batch") val start: String? = null,
     @SerialName("next_batch") val end: String? = null,
-    @SerialName("chunk") val chunk: List<@Contextual Event.RoomEvent<*>>? = null,
+    @SerialName("chunk") val chunk: List<@Contextual Event.RoomEvent<*>>,
 )
