374d7ea8bcd582d2e950c926fced270513a9e4f9
benkuly <kontakt@folivo.net>
2022-12-05 17:01:32 +0100

    allow relations without TimelineEvent

--- trixnity-client/src/commonMain/kotlin/net/folivo/trixnity/client/room/message/replace.kt
+++ trixnity-client/src/commonMain/kotlin/net/folivo/trixnity/client/room/message/replace.kt
@@ -2,11 +2,23 @@ package net.folivo.trixnity.client.room.message
 
 import net.folivo.trixnity.client.store.TimelineEvent
 import net.folivo.trixnity.core.TrixnityDsl
+import net.folivo.trixnity.core.model.EventId
+import net.folivo.trixnity.core.model.events.Event
 import net.folivo.trixnity.core.model.events.RelatesTo
 
 @TrixnityDsl
 fun MessageBuilder.replace(
     event: TimelineEvent,
+) = replace(event.eventId)
+
+@TrixnityDsl
+fun MessageBuilder.replace(
+    event: Event.MessageEvent<*>,
+) = replace(event.id)
+
+@TrixnityDsl
+fun MessageBuilder.replace(
+    eventId: EventId,
 ) {
-    relatesTo = RelatesTo.Replace(event.eventId, null)
+    relatesTo = RelatesTo.Replace(eventId, null)
 }
