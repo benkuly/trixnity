126464ea3c2126fbfda15d8ed16dfb0072b8fb77
benkuly <kontakt@folivo.net>
2023-01-04 15:11:05 +0100

    Revert "earlier sync state RUNNING for faster initial sync"
    This reverts commit 90f2a0ae4a39b5aef353035055857b6e7614eff3.

--- trixnity-clientserverapi/trixnity-clientserverapi-client/src/commonMain/kotlin/net/folivo/trixnity/clientserverapi/client/SyncApiClient.kt
+++ trixnity-clientserverapi/trixnity-clientserverapi-client/src/commonMain/kotlin/net/folivo/trixnity/clientserverapi/client/SyncApiClient.kt
@@ -38,9 +38,7 @@ enum class SyncState {
     STARTED,
 
     /**
-     * The sync is running. This is set right after [SyncResponseSubscriber]s has been called.
-     * This means that all events have been processed, which are relevant for decrypting and showing
-     * basic information to the user (e.g. user profile, room list or device verification).
+     * A normal sync has been finished. It is normally set when the sync is run in a loop.
      */
     RUNNING,
 
@@ -295,6 +293,7 @@ class SyncApiClientImpl(
         processSyncResponse(response)
         log.debug { "processed sync response with token ${currentBatchToken.value}" }
         currentBatchToken.value = response.nextBatch
+        updateSyncState(RUNNING)
         return response
     }
 
@@ -314,8 +313,6 @@ class SyncApiClientImpl(
         coroutineScope {
             syncResponseSubscribers.value.forEach { launch { it.invoke(response) } }
         }
-
-        updateSyncState(RUNNING)
 
         coroutineScope {
             launch { response.presence?.events?.forEach { emitEvent(it) } }
