f59a406ea440e87f5587b79d316e008ac9f4b638
benkuly <kontakt@folivo.net>
2023-01-17 16:54:05 +0100

    deprecate senderKey in RoomKeyRequestEventContent fix wrong naming of Curve25519KeySerializer descriptor

--- trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/model/events/m/RoomKeyRequestEventContent.kt
+++ trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/model/events/m/RoomKeyRequestEventContent.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 import net.folivo.trixnity.core.model.RoomId
 import net.folivo.trixnity.core.model.events.ToDeviceEventContent
 import net.folivo.trixnity.core.model.keys.EncryptionAlgorithm
+import net.folivo.trixnity.core.model.keys.Key
 
 /**
  * @see <a href="https://spec.matrix.org/v1.3/client-server-api/#mroom_key_request">matrix spec</a>
@@ -25,11 +26,17 @@ data class RoomKeyRequestEventContent(
     data class RequestedKeyInfo(
         @SerialName("room_id")
         val roomId: RoomId,
-        @SerialName("sender_key")
-        val senderKey: String,
         @SerialName("session_id")
         val sessionId: String,
         @SerialName("algorithm")
-        val algorithm: EncryptionAlgorithm
+        val algorithm: EncryptionAlgorithm,
+        @SerialName("sender_key")
+        @Deprecated(
+            "This field provides no additional security or privacy benefit and must not be read from. " +
+                    "It should still be included on outgoing messages (if the event for which keys are being requested for " +
+                    "also has a sender_key), however must not be used to find the corresponding session. " +
+                    "See m.megolm.v1.aes-sha2 for more information."
+        )
+        val senderKey: Key.Curve25519Key? = null
     )
 }
