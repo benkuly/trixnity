c5623bd3185c32d2aef2ddb1cb0baaf9916d4a20
benkuly <kontakt@folivo.net>
2023-06-07 16:08:24 +0200

    wip: apple crypto-core

--- trixnity-crypto-core/src/appleMain/kotlin/net/folivo/trixnity/crypto/core/pbkdf2Sha512.kt
+++ trixnity-crypto-core/src/appleMain/kotlin/net/folivo/trixnity/crypto/core/pbkdf2Sha512.kt
@@ -1,6 +1,8 @@
 package net.folivo.trixnity.crypto.core
 
+import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.convert
+import kotlinx.cinterop.usePinned
 import platform.CoreCrypto.CCKeyDerivationPBKDF
 import platform.CoreCrypto.kCCPBKDF2
 import platform.CoreCrypto.kCCPRFHmacAlgSHA512
@@ -14,17 +16,21 @@ actual suspend fun generatePbkdf2Sha512(
     keyBitLength: Int
 ): ByteArray {
     val output = ByteArray((keyBitLength / 8).takeIf { keyBitLength % 8 == 0 } ?: ((keyBitLength / 8) + 1))
-    CCKeyDerivationPBKDF(
-        // FIXME checkError
-        algorithm = kCCPBKDF2,
-        password = password,
-        passwordLen = password.length.convert(),
-        salt = salt.asUByteArray().refToOrEmpty(),
-        saltLen = salt.size.convert(),
-        prf = kCCPRFHmacAlgSHA512,
-        rounds = iterationCount.convert(),
-        derivedKey = output.asUByteArray().refToOrEmpty(),
-        derivedKeyLen = output.size.convert(),
-    )
+    salt.asUByteArray().usePinned { pinnedSalt ->
+        output.asUByteArray().usePinned { pinnedOutput ->
+            val result = CCKeyDerivationPBKDF(
+                algorithm = kCCPBKDF2,
+                password = password,
+                passwordLen = password.length.convert(),
+                salt = pinnedSalt.addressOf(0),
+                saltLen = salt.size.convert(),
+                prf = kCCPRFHmacAlgSHA512,
+                rounds = iterationCount.convert(),
+                derivedKey = pinnedOutput.addressOf(0),
+                derivedKeyLen = output.size.convert(),
+            )
+            if (result <= 0) error("unknown error with generatePbkdf2Sha512")
+        }
+    }
     return output
 }
