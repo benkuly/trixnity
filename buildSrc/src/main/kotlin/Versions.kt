49412c9a5436b1b94ce19dfa22286c52086ace68
benkuly <kontakt@folivo.net>
2023-06-08 16:32:54 +0200

    upgrade dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,12 +8,12 @@ object Versions {
     const val kotlinxCoroutines = "1.7.1" // https://github.com/Kotlin/kotlinx.coroutines/releases
     const val kotlinxSerialization = "1.5.1" // https://github.com/Kotlin/kotlinx.serialization/releases
     const val kotlinxDatetime = "0.4.0" // https://github.com/Kotlin/kotlinx-datetime/releases
-    const val dokka = "1.8.10" // https://github.com/Kotlin/dokka/releases
+    const val dokka = "1.8.20" // https://github.com/Kotlin/dokka/releases
     const val olm = "3.2.15" // https://gitlab.matrix.org/matrix-org/olm/-/releases
     const val trixnityBinaries = "1.0.0" // https://gitlab.com/trixnity/trixnity-binaries/-/releases
     const val jna = "5.13.0" // https://github.com/java-native-access/jna/tags
     const val ktor = "2.3.1" // https://github.com/ktorio/ktor/releases
-    const val koin = "3.4.1" // https://github.com/InsertKoinIO/koin/tags
+    const val koin = "3.4.2" // https://github.com/InsertKoinIO/koin/tags
     const val korlibs = "4.0.3" // https://github.com/korlibs/korge/releases
     const val mocKmp = "1.14.0" // https://github.com/Kodein-Framework/MocKMP/releases
     const val okio = "3.3.0" // https://square.github.io/okio/changelog/
@@ -30,7 +30,6 @@ object Versions {
     const val androidxTextKtx = "1.5.0" // https://developer.android.com/jetpack/androidx/releases/test
     const val robolectric = "4.10.3" // https://github.com/robolectric/robolectric/releases
     const val downloadGradlePlugin = "5.4.0" // https://github.com/michel-kraemer/gradle-download-task/releases
-    const val completeKotlinPlugin = "1.1.0" // https://github.com/LouisCAD/CompleteKotlin/releases
 
     // upgrade only, as soon as https://github.com/tony19/logback-android/issues/249 is resolved
     const val kotlinLogging = "2.1.23" // https://github.com/MicroUtils/kotlin-logging/releases
