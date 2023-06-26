1a9690a7e95c8a8d981ebc840bb229a8422da4e9
benkuly <kontakt@folivo.net>
2023-06-26 15:56:35 +0200

    upgrade dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 object Versions {
     val kotlinJvmTarget = JavaVersion.VERSION_11
-    const val ksp = "1.8.21-1.0.11" // https://github.com/google/ksp/releases
+    const val ksp = "1.8.22-1.0.11" // https://github.com/google/ksp/releases
     const val kotlinxCoroutines = "1.7.1" // https://github.com/Kotlin/kotlinx.coroutines/releases
     const val kotlinxSerialization = "1.5.1" // https://github.com/Kotlin/kotlinx.serialization/releases
     const val kotlinxDatetime = "0.4.0" // https://github.com/Kotlin/kotlinx-datetime/releases
@@ -14,7 +14,7 @@ object Versions {
     const val jna = "5.13.0" // https://github.com/java-native-access/jna/tags
     const val ktor = "2.3.1" // https://github.com/ktorio/ktor/releases
     const val koin = "3.4.2" // https://github.com/InsertKoinIO/koin/tags
-    const val korlibs = "4.0.3" // https://github.com/korlibs/korge/releases
+    const val korlibs = "4.0.6" // https://github.com/korlibs/korge/releases
     const val mocKmp = "1.14.0" // https://github.com/Kodein-Framework/MocKMP/releases
     const val okio = "3.3.0" // https://square.github.io/okio/changelog/
     const val uuid = "0.7.1" // https://github.com/benasher44/uuid/releases
@@ -26,7 +26,7 @@ object Versions {
     const val juulLabsIndexeddb = "0.6.0" // https://github.com/JuulLabs/indexeddb/releases
     const val h2 = "2.1.214" // https://github.com/h2database/h2database/releases
     const val realm = "1.9.1" // https://github.com/realm/realm-kotlin/tags
-    const val androidxRoom = "2.5.1" // https://developer.android.com/jetpack/androidx/releases/room
+    const val androidxRoom = "2.5.2" // https://developer.android.com/jetpack/androidx/releases/room
     const val androidxTextKtx = "1.5.0" // https://developer.android.com/jetpack/androidx/releases/test
     const val robolectric = "4.10.3" // https://github.com/robolectric/robolectric/releases
     const val downloadGradlePlugin = "5.4.0" // https://github.com/michel-kraemer/gradle-download-task/releases
