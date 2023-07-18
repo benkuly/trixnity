fc8db18f225743ee4a6f0d72a236390faff7c635
benkuly <kontakt@folivo.net>
2023-07-18 17:50:14 +0200

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 
 allprojects {
     group = "net.folivo"
-    version = withVersionSuffix("3.7.1")
+    version = withVersionSuffix("3.7.2")
 
     repositories {
         mavenCentral()
