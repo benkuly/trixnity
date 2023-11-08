127708bbb2e15b606f8f0142f294cec795b02814
Benedict <3976820-benkuly@users.noreply.gitlab.com>
2023-11-08 18:59:17 +0000

    Update release.kt

--- buildSrc/src/main/kotlin/release.kt
+++ buildSrc/src/main/kotlin/release.kt
@@ -1,14 +1,14 @@
 val isCI = System.getenv("CI") != null
 val isRelease = System.getenv("CI_COMMIT_TAG")?.matches("^v\\d+.\\d+.\\d+.*".toRegex()) ?: false
-fun withVersionSuffix(version: String) = (version + when {
+fun withVersionSuffix(version: String) = when {
     isRelease -> {
         val commitTagVersion = System.getenv("CI_COMMIT_TAG").removePrefix("v")
         check(version == commitTagVersion.substringBefore("-")) {
             "version from code ($version) does not match commit tag version ($commitTagVersion)"
         }
-        commitTagVersion.substringAfter("-")
+        commitTagVersion
     }
 
-    isCI -> "-SNAPSHOT-" + System.getenv("CI_COMMIT_SHORT_SHA")
-    else -> "-LOCAL"
-})
+    isCI -> "$version-SNAPSHOT-" + System.getenv("CI_COMMIT_SHORT_SHA")
+    else -> "$version-LOCAL"
+}
