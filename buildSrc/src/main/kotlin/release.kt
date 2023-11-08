a968384c708086149102595999d5c8c6dd5d5411
benkuly <kontakt@folivo.net>
2023-11-08 17:38:02 +0100

    allow to publish beta from commit tag

--- buildSrc/src/main/kotlin/release.kt
+++ buildSrc/src/main/kotlin/release.kt
@@ -1,11 +1,14 @@
 val isCI = System.getenv("CI") != null
 val isRelease = System.getenv("CI_COMMIT_TAG")?.matches("^v\\d+.\\d+.\\d+.*".toRegex()) ?: false
-fun checkSameReleaseVersion(version: String) {
-    if (isRelease) check(version == System.getenv("CI_COMMIT_TAG").removePrefix("v"))
-}
-
 fun withVersionSuffix(version: String) = (version + when {
-    isRelease -> ""
+    isRelease -> {
+        val commitTagVersion = System.getenv("CI_COMMIT_TAG").removePrefix("v")
+        check(version == commitTagVersion.substringBefore("-")) {
+            "version from code ($version) does not match commit tag version ($commitTagVersion)"
+        }
+        commitTagVersion
+    }
+
     isCI -> "-SNAPSHOT-" + System.getenv("CI_COMMIT_SHORT_SHA")
     else -> "-LOCAL"
-}).also { checkSameReleaseVersion(it) }
+})
