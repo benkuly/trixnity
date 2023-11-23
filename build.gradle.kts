4b9732116d7fd08ac0312b89cd31b24a9e8bb95d
benkuly <kontakt@folivo.net>
2023-11-23 18:04:39 +0100

    skip dokka when not in CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ subprojects {
             dependsOn(tasks.dokkaHtml)
             from(tasks.dokkaHtml.flatMap { it.outputDirectory })
             archiveClassifier.set("javadoc")
+            onlyIf { isCI }
         }
 
         publishing {
@@ -75,7 +76,7 @@ subprojects {
                             url.set("https://gitlab.com/trixnity/trixnity")
                         }
                     }
-                    artifact(dokkaJar)
+                    if (isCI) artifact(dokkaJar)
                 }
             }
         }
