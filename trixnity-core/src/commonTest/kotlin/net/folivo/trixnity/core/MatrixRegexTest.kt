01bc70501393f3d3203950cb04fa5e6f592903bf
Adam Brangenberg <adambrangenberg@proton.me>
2024-02-13 20:10:04 +0100

    formatting, adding more tests

--- trixnity-core/src/commonTest/kotlin/net/folivo/trixnity/core/MatrixRegexTest.kt
+++ trixnity-core/src/commonTest/kotlin/net/folivo/trixnity/core/MatrixRegexTest.kt
@@ -6,107 +6,84 @@ import kotlin.test.Test
 
 
 class MatrixRegexTest {
+    fun positiveTest(id: String, localpart: String, domain: String) {
+        val message = "Hello $id"
 
-    @Test
-    fun matchValidUserIdentifier() {
-        val message = "Hello @u1ser:example.com"
         val result = matchUsers(message)
         result.size shouldBe 1
-        result["@u1ser:example.com"] shouldBe UserId("u1ser", "example.com")
+        println(result)
+        println(id)
+        result[id] shouldBe UserId(localpart, domain)
+    }
+
+    fun negativeTest(id: String) {
+        val message = "Hello $id"
+
+        val result = matchUsers(message)
+        result.size shouldBe 0
     }
 
     @Test
-        fun matchValidUserIdentifierWithMatrixToLink() {
-            val message = "Hello <a href=\"https://matrix.to/#/@user:example.com\">Hallo</a>"
-            val result = matchUsers(message)
-            result.size shouldBe 1
-            result["<a href=\"https://matrix.to/#/@user:example.com\">Hallo</a>"]
-                .shouldBe(UserId("user", "example.com"))
-        }
+    fun matchValidUserIdentifier() {
+        positiveTest("@a9._=-/+:example.com", "a9._=-/+", "example.com")
+    }
 
-        @Test
-        fun matchValidUserIdentifierWithMatrixToLinkWithoutHref() {
-            val message = "Hello https://matrix.to/#/@user:example.com"
-            val result = matchUsers(message)
-            result.size shouldBe 1
-            result["https://matrix.to/#/@user:example.com"]
-                .shouldBe(UserId("user", "example.com"))
-        }
+    @Test
+    fun matchValidUserIdentifierWithMatrixToLink() {
+        positiveTest("<a href=\"https://matrix.to/#/@user:example.com\">Hallo</a>", "user", "example.com")
+    }
 
-        @Test
-        fun matchValidUserIdentifierWithMatrixULinkAndActionAttribute() {
-            val message = "Hello matrix:u/user:example.com?action=chat"
-            val result = matchUsers(message)
-            result.size shouldBe 1
-            result["matrix:u/user:example.com?action=chat"]
-                .shouldBe(UserId("user", "example.com"))
-        }
+    @Test
+    fun matchValidUserIdentifierWithMatrixToLinkWithoutHref() {
+        positiveTest("https://matrix.to/#/@user:example.com", "user", "example.com")
+    }
 
-        @Test
-        fun matchValidUserIdentifierWithMatrixULinkAndViaAttribute() {
-            val message = "Hello matrix:u/user:example.com?via=example.com"
-            val result = matchUsers(message)
-            result.size shouldBe 1
-            result["matrix:u/user:example.com?via=example.com"]
-                .shouldBe(UserId("user", "example.com"))
-        }
+    @Test
+    fun matchValidUserIdentifierWithMatrixULinkAndActionAttribute() {
+        positiveTest("matrix:u/user:example.com?action=chat", "user", "example.com")
+    }
 
-        @Test
-        fun matchValidUserIdentifierWithMatrixULinkViaAndActionAttribute() {
-            val message1 = "Hello matrix:u/user:example.com?action=chat&via=example.com"
-            val result1 = matchUsers(message1)
-            result1.size shouldBe 1
-            result1["matrix:u/user:example.com?action=chat&via=example.com"]
-                .shouldBe(UserId("user", "example.com"))
+    @Test
+    fun matchValidUserIdentifierWithMatrixULinkAndViaAttribute() {
+        positiveTest("matrix:u/user:example.com?via=example.com", "user", "example.com")
+    }
 
-            val message2 = "Hello matrix:u/user:example.com?via=example.com&action=chat"
-            val result2 = matchUsers(message2)
-            result2.size shouldBe 1
-            result2["matrix:u/user:example.com?via=example.com&action=chat"]
-                .shouldBe(UserId("user", "example.com"))
-        }
+    @Test
+    fun matchValidUserIdentifierWithMatrixULinkViaAndActionAttribute() {
+        positiveTest("matrix:u/user:example.com?via=example.com&action=chat", "user", "example.com")
+    }
 
-        @Test
-        fun matchValidUserIdentifierWithMatrixULink() {
-            val message = "Hello matrix:u/user:example.com"
-            val result = matchUsers(message)
-            result.size shouldBe 1
-            result["matrix:u/user:example.com"]
-                .shouldBe(UserId("user", "example.com"))
-        }
+    fun matchValidUserIdentifierWithMatrixULinkActionAndViaAttribute() {
+        positiveTest("matrix:u/user:example.com?action=chat&via=example.com", "user", "example.com")
+    }
 
-        @Test
-        fun matchValidUserIdentifierWithSpecialCharacters() {
-            val message = "Hello @a9._=-/+:sub.example.com:8000"
-            val result = matchUsers(message)
-            result.size shouldBe 1
-            result["@a9._=-/+:sub.example.com:8000"]
-                .shouldBe(UserId("a9._=-/+", "sub.example.com:8000"))
-        }
+    @Test
+    fun matchValidUserIdentifierWithMatrixULink() {
+        positiveTest("matrix:u/user:example.com", "user", "example.com")
+    }
 
-        @Test
-        fun matchValidUserIdentifierWithIPV4() {
-            val message = "Hello @a9._=-/+:1.1.1.1"
-            val result = matchUsers(message)
-            result.size shouldBe 1
-            result["@a9._=-/+:1.1.1.1"]
-                .shouldBe(UserId("a9._=-/+", "1.1.1.1"))
-        }
+    @Test
+    fun matchValidUserIdentifierWithSpecialCharacters() {
+        positiveTest("@user:sub.example.com:8000", "user", "sub.example.com:8000")
+    }
 
-        @Test
-        fun matchValidUserIdentifierWithIPV6() {
-            val message = "Hello @a9._=-/+:[2001:0db8:85a3:0000:0000:8a2e:0370:7334]"
-            val result = matchUsers(message)
-            result.size shouldBe 1
-            result["@a9._=-/+:[2001:0db8:85a3:0000:0000:8a2e:0370:7334]"]
-                .shouldBe(UserId("a9._=-/+", "[2001:0db8:85a3:0000:0000:8a2e:0370:7334]"))
-        }
+    @Test
+    fun matchValidUserIdentifierWithIPV4() {
+        positiveTest("@user:1.1.1.1", "user", "1.1.1.1")
+    }
 
-        @Test
-        fun notMatchInvalidUserIdentifier() {
-            val message = "Hello @user&:ex&mple.com"
-            val result = matchUsers(message)
-            result.size shouldBe 0
-        }
+    @Test
+    fun matchValidUserIdentifierWithIPV6() {
+        positiveTest("@user:[2001:0db8:85a3:0000:0000:8a2e:0370:7334]", "user", "2001:0db8:85a3:0000:0000:8a2e:0370:7334")
+    }
 
+    @Test
+    fun notMatchInvalidLocalpart() {
+        negativeTest("@user&:example.com")
+    }
+
+    @Test
+    fun notMatchInvalidDomain() {
+        negativeTest("@user:ex&mple.com")
+    }
 }
