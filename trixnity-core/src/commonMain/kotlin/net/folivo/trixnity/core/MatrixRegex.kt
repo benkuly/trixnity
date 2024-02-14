d6772124936646e7cb2b9c1ba5ca2861b5559348
Adam Brangenberg <adambrangenberg@proton.me>
2024-02-14 17:54:03 +0100

    Moving baseRegexes into MatrixRegex

--- trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/MatrixRegex.kt
+++ trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/MatrixRegex.kt
@@ -2,32 +2,32 @@ package net.folivo.trixnity.core
 
 import net.folivo.trixnity.core.model.UserId
 
-// https://spec.matrix.org/v1.9/appendices/#identifier-grammar
-private const val baseLocalpartRegex = """[a-z0-9.\-_=\/+]+"""
-
-// https://spec.matrix.org/v1.9/appendices/#server-name
-private const val basePortRegex = """:[0-9]{1,5}"""
-private const val baseDomainRegex = """(?:[\w-]+\.)?[\w-]+\.[\w-]+(?:$basePortRegex)?"""
-private const val baseIPV4Regex = """\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:$basePortRegex)?"""
-private const val baseIPV6Regex = """\[[0-9a-fA-F:]+\](?:$basePortRegex)?"""
-private const val baseServernameRegex = """(?:(?:$baseIPV4Regex)|(?:$baseDomainRegex)|(?:$baseIPV6Regex))"""
-
-// https://spec.matrix.org/v1.9/appendices/#user-identifiers
-private const val baseUserIdRegex = """@($baseLocalpartRegex):($baseServernameRegex)"""
-
-// https://spec.matrix.org/v1.9/appendices/#matrix-uri-scheme
-private const val baseUserUriRegex =
-    """matrix:u\/($baseLocalpartRegex):($baseServernameRegex)(?:(?:\?action=chat(?:&via=$baseServernameRegex)?)|(?:\?via=$baseServernameRegex(?:&action=chat)?))?"""
-
-// https://spec.matrix.org/v1.9/appendices/#matrixto-navigation
-private const val baseUserLinkRegex = """https?:\/\/matrix\.to\/#\/@($baseLocalpartRegex):($baseServernameRegex)"""
-private const val baseUserHtmlAnchorRegex = """<a href="$baseUserLinkRegex">.*<\/a>"""
-
-private const val baseMentionUserRegex =
-    """(?:(?:$baseUserIdRegex)|(?:$baseUserUriRegex)|(?:$baseUserLinkRegex)|(?:$baseUserHtmlAnchorRegex))$"""
-
-
 object MatrixRegex {
+
+    // https://spec.matrix.org/v1.9/appendices/#identifier-grammar
+    private const val baseLocalpartRegex = """[a-z0-9.\-_=\/+]+"""
+
+    // https://spec.matrix.org/v1.9/appendices/#server-name
+    private const val basePortRegex = """:[0-9]{1,5}"""
+    private const val baseDomainRegex = """(?:[\w-]+\.)?[\w-]+\.[\w-]+(?:$basePortRegex)?"""
+    private const val baseIPV4Regex = """\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:$basePortRegex)?"""
+    private const val baseIPV6Regex = """\[[0-9a-fA-F:]+\](?:$basePortRegex)?"""
+    private const val baseServernameRegex = """(?:(?:$baseIPV4Regex)|(?:$baseDomainRegex)|(?:$baseIPV6Regex))"""
+
+    // https://spec.matrix.org/v1.9/appendices/#user-identifiers
+    private const val baseUserIdRegex = """@($baseLocalpartRegex):($baseServernameRegex)"""
+
+    // https://spec.matrix.org/v1.9/appendices/#matrix-uri-scheme
+    private const val baseUserUriRegex =
+        """matrix:u\/($baseLocalpartRegex):($baseServernameRegex)(?:(?:\?action=chat(?:&via=$baseServernameRegex)?)|(?:\?via=$baseServernameRegex(?:&action=chat)?))?"""
+
+    // https://spec.matrix.org/v1.9/appendices/#matrixto-navigation
+    private const val baseUserLinkRegex = """https?:\/\/matrix\.to\/#\/@($baseLocalpartRegex):($baseServernameRegex)"""
+    private const val baseUserHtmlAnchorRegex = """<a href="$baseUserLinkRegex">.*<\/a>"""
+
+    private const val baseMentionUserRegex =
+        """(?:(?:$baseUserIdRegex)|(?:$baseUserUriRegex)|(?:$baseUserLinkRegex)|(?:$baseUserHtmlAnchorRegex))$"""
+
     val userMentionRegex by lazy { baseMentionUserRegex.toRegex() }
     val domainRegex by lazy { baseDomainRegex.toRegex() }
     val localpartRegex by lazy { baseLocalpartRegex.toRegex() }
