670684dc9304b393f453c7d0a048b040cbefe61c
benkuly <kontakt@folivo.net>
2024-02-15 11:34:47 +0100

    upgrade dependencies

--- trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/MatrixRegex.kt
+++ trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/MatrixRegex.kt
@@ -5,36 +5,37 @@ import net.folivo.trixnity.core.model.UserId
 object MatrixRegex {
 
     // https://spec.matrix.org/v1.9/appendices/#identifier-grammar
-    private const val baseLocalpartRegex = """[a-z0-9.\-_=\/+]+"""
+    private const val baseUserLocalpartRegex = """[a-z0-9.\-_=/+]+"""
 
     // https://spec.matrix.org/v1.9/appendices/#server-name
     private const val basePortRegex = """:[0-9]{1,5}"""
-    private const val baseDomainRegex = """(?:[\w-]+\.)?[\w-]+\.[\w-]+(?:$basePortRegex)?"""
+    private const val baseDnsRegex = """(?:[\w-]+\.)?[\w-]+\.[\w-]+(?:$basePortRegex)?"""
     private const val baseIPV4Regex = """\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:$basePortRegex)?"""
     private const val baseIPV6Regex = """\[[0-9a-fA-F:]+\](?:$basePortRegex)?"""
-    private const val baseServernameRegex = """(?:(?:$baseIPV4Regex)|(?:$baseDomainRegex)|(?:$baseIPV6Regex))"""
+    private const val baseDomainRegex = """(?:(?:$baseIPV4Regex)|(?:$baseDnsRegex)|(?:$baseIPV6Regex))"""
 
     // https://spec.matrix.org/v1.9/appendices/#user-identifiers
-    private const val baseUserIdRegex = """@($baseLocalpartRegex):($baseServernameRegex)"""
+    private const val baseUserIdRegex = """@($baseUserLocalpartRegex):($baseDomainRegex)"""
 
     // https://spec.matrix.org/v1.9/appendices/#matrix-uri-scheme
     private const val baseUserUriRegex =
-        """matrix:u\/($baseLocalpartRegex):($baseServernameRegex)(?:(?:\?action=chat(?:&via=$baseServernameRegex)?)|(?:\?via=$baseServernameRegex(?:&action=chat)?))?"""
+        """matrix:u/($baseUserLocalpartRegex):($baseDomainRegex)(?:(?:\?action=chat(?:&via=$baseDomainRegex)?)|(?:\?via=$baseDomainRegex(?:&action=chat)?))?"""
 
     // https://spec.matrix.org/v1.9/appendices/#matrixto-navigation
-    private const val baseUserLinkRegex = """https?:\/\/matrix\.to\/#\/@($baseLocalpartRegex):($baseServernameRegex)"""
-    private const val baseUserHtmlAnchorRegex = """<a href="$baseUserLinkRegex">.*<\/a>"""
+    private const val baseUserLinkRegex = """https?://matrix\.to/#/@($baseUserLocalpartRegex):($baseDomainRegex)"""
+    private const val baseUserHtmlLinkRegex = """<a href="$baseUserLinkRegex">.*</a>"""
 
-    private const val baseMentionUserRegex =
-        """(?:(?:$baseUserIdRegex)|(?:$baseUserUriRegex)|(?:$baseUserLinkRegex)|(?:$baseUserHtmlAnchorRegex))$"""
+    private const val baseUserMentionRegex =
+        """(?:(?:$baseUserIdRegex)|(?:$baseUserUriRegex)|(?:$baseUserLinkRegex)|(?:$baseUserHtmlLinkRegex))$"""
 
-    val userMention by lazy { baseMentionUserRegex.toRegex() }
-    val domain by lazy { baseServernameRegex.toRegex() }
-    val localpart by lazy { baseLocalpartRegex.toRegex() }
+    val domain by lazy { baseDomainRegex.toRegex() }
+    
+    val userLocalpart by lazy { baseUserLocalpartRegex.toRegex() }
     val userId by lazy { baseUserIdRegex.toRegex() }
     val userUri by lazy { baseUserUriRegex.toRegex() }
     val userLink by lazy { baseUserLinkRegex.toRegex() }
-    val userHtmlAnchor by lazy { baseUserHtmlAnchorRegex.toRegex() }
+    val userHtmlLink by lazy { baseUserHtmlLinkRegex.toRegex() }
+    val userMention by lazy { baseUserMentionRegex.toRegex() }
 
     fun findUserMentions(message: String): Map<String, UserId> {
         val matches = userMention.findAll(message)
