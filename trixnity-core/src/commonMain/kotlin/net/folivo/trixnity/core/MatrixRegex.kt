931b53afa20e546c4c7d9baa3ef6bdeea8c3fd09
Adam Brangenberg <adambrangenberg@proton.me>
2024-02-22 17:29:32 +0100

    Adding Room Regex

--- trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/MatrixRegex.kt
+++ trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/MatrixRegex.kt
@@ -7,6 +7,9 @@ object MatrixRegex {
     // https://spec.matrix.org/v1.9/appendices/#identifier-grammar
     private const val baseLocalpartRegex = """[a-z0-9.\-_=\/+]+"""
 
+    // Nothing in the Appendix found, assumed that based on research
+    private const val baseOpaqueIdRegex = """[a-zA-Z0-9.\-_=\/+]+"""
+
     // https://spec.matrix.org/v1.9/appendices/#server-name
     private const val basePortRegex = """:[0-9]{1,5}"""
     private const val baseDomainRegex = """(?:[\w-]+\.)?[\w-]+\.[\w-]+(?:$basePortRegex)?"""
@@ -14,27 +17,50 @@ object MatrixRegex {
     private const val baseIPV6Regex = """\[[0-9a-fA-F:]+\](?:$basePortRegex)?"""
     private const val baseServernameRegex = """(?:(?:$baseIPV4Regex)|(?:$baseDomainRegex)|(?:$baseIPV6Regex))"""
 
+    // https://spec.matrix.org/v1.9/appendices/#room-ids
+    private const val baseRoomIdRegex = """!($baseOpaqueIdRegex):($baseServernameRegex)"""
+
+    // https://spec.matrix.org/v1.9/appendices/#room-aliases
+    private const val baseRoomAliasRegex = """#($baseLocalpartRegex):($baseServernameRegex)"""
+
+    // https://spec.matrix.org/latest/appendices/#event-ids
+    private const val baseEventIdRegex = """$($baseOpaqueIdRegex)"""
+
     // https://spec.matrix.org/v1.9/appendices/#user-identifiers
     private const val baseUserIdRegex = """@($baseLocalpartRegex):($baseServernameRegex)"""
 
     // https://spec.matrix.org/v1.9/appendices/#matrix-uri-scheme
+    private const val baseRoomUriRegex =
+        """matrix:(r|(?:roomid))\/($baseLocalpartRegex):($baseServernameRegex)(?:/e/($baseOpaqueIdRegex))?(?:(?:\?action=chat(?:&via=$baseServernameRegex)?)|(?:\?via=$baseServernameRegex(?:&action=chat)?))?"""
     private const val baseUserUriRegex =
         """matrix:u\/($baseLocalpartRegex):($baseServernameRegex)(?:(?:\?action=chat(?:&via=$baseServernameRegex)?)|(?:\?via=$baseServernameRegex(?:&action=chat)?))?"""
 
     // https://spec.matrix.org/v1.9/appendices/#matrixto-navigation
+    private const val baseRoomLinkRegex = """https?:\/\/matrix\.to\/#\/(#|!)($baseLocalpartRegex):($baseServernameRegex)(?:/($baseEventIdRegex):(?:$baseServernameRegex))?(?:\?via=($baseServernameRegex))"""
+    private const val baseRoomHtmlAnchorRegex = """<a href="$baseRoomLinkRegex">.*<\/a>"""
     private const val baseUserLinkRegex = """https?:\/\/matrix\.to\/#\/@($baseLocalpartRegex):($baseServernameRegex)"""
     private const val baseUserHtmlAnchorRegex = """<a href="$baseUserLinkRegex">.*<\/a>"""
 
+    private const val baseMentionRoomRegex =
+        """(?:(?:$baseRoomIdRegex)|(?:$baseRoomAliasRegex)|(?:$baseRoomUriRegex)|(?:$baseRoomLinkRegex)|(?:$baseRoomHtmlAnchorRegex))${'$'}"""
     private const val baseMentionUserRegex =
         """(?:(?:$baseUserIdRegex)|(?:$baseUserUriRegex)|(?:$baseUserLinkRegex)|(?:$baseUserHtmlAnchorRegex))$"""
 
-    val userMention by lazy { baseMentionUserRegex.toRegex() }
     val domain by lazy { baseServernameRegex.toRegex() }
     val localpart by lazy { baseLocalpartRegex.toRegex() }
+
+    val roomId by lazy { baseRoomIdRegex.toRegex() }
+    val roomAlias by lazy { baseRoomAliasRegex.toRegex() }
+    val roomUri by lazy { baseRoomUriRegex.toRegex() }
+    val roomLink by lazy { baseRoomLinkRegex.toRegex() }
+    val roomHtmlAnchor by lazy { baseRoomHtmlAnchorRegex.toRegex() }
+    val roomMention by lazy { baseMentionRoomRegex.toRegex() }
+
     val userId by lazy { baseUserIdRegex.toRegex() }
     val userUri by lazy { baseUserUriRegex.toRegex() }
     val userLink by lazy { baseUserLinkRegex.toRegex() }
     val userHtmlAnchor by lazy { baseUserHtmlAnchorRegex.toRegex() }
+    val userMention by lazy { baseMentionUserRegex.toRegex() }
 
     fun findUserMentions(message: String): Map<String, UserId> {
         val matches = userMention.findAll(message)
