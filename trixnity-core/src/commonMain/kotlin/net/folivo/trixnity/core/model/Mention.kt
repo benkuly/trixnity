16c08fe6e153c0727b5256ad017ea667c37ae304
Adam Brangenberg <adambrangenberg@proton.me>
2024-04-04 14:56:32 +0200

    formatting

--- trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/model/Mention.kt
+++ trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/model/Mention.kt
@@ -3,7 +3,7 @@ package net.folivo.trixnity.core.model
 sealed interface Mention {
     data class User(val userId: UserId) : Mention
     data class Room(val roomId: RoomId) : Mention
-    data class RoomEvent(val eventId: EventId, val roomId: RoomId) : Mention
+    data class RoomEvent(val roomId: RoomId, val eventId: EventId) : Mention
     data class RoomAlias(val roomAliasId: RoomAliasId) : Mention
-    data class RoomAliasEvent(val eventId: EventId, val roomAliasId: RoomAliasId) : Mention
+    data class RoomAliasEvent(val roomAliasId: RoomAliasId, val eventId: EventId) : Mention
 }
