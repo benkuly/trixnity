035d464c7359a7a3eddcbb87683d00455522e518
benkuly <kontakt@folivo.net>
2024-05-06 11:59:27 +0200

    openssl without zlib

--- buildSrc/src/main/kotlin/binaries.kt
+++ buildSrc/src/main/kotlin/binaries.kt
@@ -16,9 +16,10 @@ class TrixnityOlmBinariesDirs(project: Project, version: String) {
 }
 
 class TrixnityOpensslBinariesDirs(project: Project, version: String) {
-    private val rootDir = project.rootProject.layout.buildDirectory.get().asFile
-        .resolve("trixnity-openssl-binaries").resolve(version).resolve("openssl")
+    val root = project.rootProject.layout.buildDirectory.get().asFile
+        .resolve("trixnity-openssl-binaries").resolve(version)
 
+    private val rootDir = root.resolve("openssl")
     private fun targetDir(target: KonanTarget) = rootDir.resolve(target.name)
     fun include(target: KonanTarget) = targetDir(target).resolve("include")
     fun lib(target: KonanTarget) = targetDir(target).resolve("lib")
