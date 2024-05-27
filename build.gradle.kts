760fbe385163d196ee9ac673bdaf608890c0e35c
benkuly <kontakt@folivo.net>
2024-05-27 11:53:25 +0200

    version downgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 allprojects {
     group = "net.folivo"
-    version = withVersionSuffix("4.3.5")
+    version = withVersionSuffix("4.3.4")
 
     dependencyLocking {
         lockAllConfigurations()
