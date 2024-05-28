9ccaa4498d73b7daf78bb645a5ca0fbca7270eab
benkuly <kontakt@folivo.net>
2024-05-28 12:06:11 +0200

    Revert "bump version"
    This reverts commit 870279f85d422281ea60f6961c4cc47951d0e46c.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 allprojects {
     group = "net.folivo"
-    version = withVersionSuffix("4.3.7")
+    version = withVersionSuffix("4.3.6")
 
     dependencyLocking {
         lockAllConfigurations()
