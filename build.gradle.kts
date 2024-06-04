bf1512cb4b48380a92ce516b0e7d31cd4030413e
benkuly <kontakt@folivo.net>
2024-06-04 16:42:24 +0200

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 allprojects {
     group = "net.folivo"
-    version = withVersionSuffix("4.3.8")
+    version = withVersionSuffix("4.3.9")
 
     dependencyLocking {
         lockAllConfigurations()
