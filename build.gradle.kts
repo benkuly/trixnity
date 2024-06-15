480ed5dc04b57211fd75cab4115dd647e04a9b09
benkuly <kontakt@folivo.net>
2024-06-15 07:54:39 +0200

    version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 allprojects {
     group = "net.folivo"
-    version = withVersionSuffix("4.4.0")
+    version = withVersionSuffix("4.3.11")
 
     dependencyLocking {
         lockAllConfigurations()
