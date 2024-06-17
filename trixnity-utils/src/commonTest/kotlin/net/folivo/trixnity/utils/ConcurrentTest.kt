eac7c9a8fdc4140ce61937d8a2528514d5e31cf2
benkuly <kontakt@folivo.net>
2024-06-17 10:14:52 +0200

    make test more parallel

--- trixnity-utils/src/commonTest/kotlin/net/folivo/trixnity/utils/ConcurrentTest.kt
+++ trixnity-utils/src/commonTest/kotlin/net/folivo/trixnity/utils/ConcurrentTest.kt
@@ -3,21 +3,16 @@ package net.folivo.trixnity.utils
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.getAndUpdate
 import kotlinx.coroutines.test.runTest
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.time.measureTime
 
 class ConcurrentTest {
-    private lateinit var cut: Concurrent<List<Int>, MutableList<Int>>
-
-    @BeforeTest
-    fun before() {
-        cut = concurrentOf { mutableListOf() }
-    }
-
     @Test
     fun writeAndReadData() = runTest {
+        val cut = concurrentMutableList<Int>()
         cut.write { add(1) }
         cut.write { add(2) }
         cut.read { toList() } shouldBe listOf(1, 2)
@@ -25,6 +20,7 @@ class ConcurrentTest {
 
     @Test
     fun throwExceptionsOnLeak() = runTest {
+        val cut = concurrentMutableList<Int>()
         shouldThrow<IllegalArgumentException> {
             cut.write { this }
         }
@@ -35,15 +31,21 @@ class ConcurrentTest {
 
     @Test
     fun massiveReadWriteShouldNotThrowConcurrentModificationException() = runTest {
+        val cut = concurrentMutableList<Int>()
         withContext(Dispatchers.Default) {
-            val writeOperations = 100
+            val parallel = 10
+            val writeOperations = 200
             val readOperations = writeOperations * 100
             val writeTime = async {
+                val numbers = MutableStateFlow(0)
                 measureTime {
                     coroutineScope {
-                        repeat(writeOperations) { i ->
+                        repeat(parallel) {
                             launch {
-                                cut.write { add(i) }
+                                repeat(writeOperations / parallel) {
+                                    val number = numbers.getAndUpdate { it + 1 }
+                                    cut.write { add(number) }
+                                }
                             }
                         }
                     }
@@ -52,9 +54,11 @@ class ConcurrentTest {
             val readTime = async {
                 measureTime {
                     coroutineScope {
-                        repeat(readOperations) {
+                        repeat(parallel) {
                             launch {
-                                cut.read { toSet() }
+                                repeat(readOperations / parallel) {
+                                    cut.read { toSet() }
+                                }
                             }
                         }
                     }
@@ -65,6 +69,32 @@ class ConcurrentTest {
                         "readTime=${readTime.await()} (${readTime.await() / readOperations}/operation)"
             )
             cut.read { toList() }.sorted() shouldBe List(writeOperations) { it }
+        }
+    }
+
+    @Test
+    fun getOrPut() = runTest {
+        val cut = concurrentMutableMap<String, String>()
+        val parallel = 10
+        val writeOperations = 200
+        val readOperations = writeOperations * 100
+
+        cut.write { getOrPut("key1") { "value" } } shouldBe "value"
+        cut.write { getOrPut("key2") { "value" } } shouldBe "value"
+
+        repeat(parallel) {
+            launch {
+                repeat(writeOperations / parallel) {
+                    cut.write { getOrPut("key1") { "value$it" } } shouldBe "value"
+                    cut.write { getOrPut("key2") { "value$it" } } shouldBe "value"
+                }
+            }
+            launch {
+                repeat(readOperations / parallel) {
+                    cut.read { get("key1") } shouldBe "value"
+                    cut.read { get("key2") } shouldBe "value"
+                }
+            }
         }
     }
 }
