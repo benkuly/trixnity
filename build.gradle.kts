e10627a9ddf4f46d5348a87d45ef12160d3c040d
benkuly <kontakt@folivo.net>
2024-07-30 10:32:40 +0200

    revert bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 allprojects {
     group = "net.folivo"
-    version = withVersionSuffix("4.6.2")
+    version = withVersionSuffix("4.6.1")
 
     if (System.getenv("WITH_LOCK")?.toBoolean() == true) {
         dependencyLocking {
