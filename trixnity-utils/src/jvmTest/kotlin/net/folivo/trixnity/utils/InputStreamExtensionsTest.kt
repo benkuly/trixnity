b53d3d657db5b11e73bdde1078488cfb650d975e
Felix Hilgers <contact@fhilgers.com>
2024-10-02 18:44:17 +0200

    fix: invalid test

--- trixnity-utils/src/jvmTest/kotlin/net/folivo/trixnity/utils/InputStreamExtensionsTest.kt
+++ trixnity-utils/src/jvmTest/kotlin/net/folivo/trixnity/utils/InputStreamExtensionsTest.kt
@@ -2,6 +2,9 @@ package net.folivo.trixnity.utils
 
 import io.kotest.core.spec.style.ShouldSpec
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.onEach
+import java.io.InputStream
+import java.io.OutputStream
 import java.io.PipedInputStream
 import java.io.PipedOutputStream
 import kotlin.test.assertContentEquals
@@ -25,18 +28,28 @@ class InputStreamExtensionsTest : ShouldSpec() {
             }
             should("work with InputStreams where data is not available yet") {
                 val outputStream = PipedOutputStream()
-                val byteArrayFlow = byteArrayFlowFromInputStream { PipedInputStream(outputStream) }
+                var inputStream: InputStream? = PipedInputStream(outputStream)
+
+                val byteArrayFlow = byteArrayFlowFromInputStream {
+                    inputStream?.let {
+                        inputStream = null
+                        it
+                    } ?: throw Exception("this stream cannot be subscribed twice")
+                }
 
                 val write = async(Dispatchers.IO, start = CoroutineStart.LAZY) {
+                    delay(10.milliseconds)
                     outputStream.write("he".toByteArray())
                     delay(10.milliseconds)
                     outputStream.write("llo".toByteArray())
                     delay(10.milliseconds)
                     outputStream.close()
                 }
+
                 val read = async(Dispatchers.IO, start = CoroutineStart.LAZY) {
                     assertContentEquals("hello".toByteArray(), byteArrayFlow.toByteArray())
                 }
+
                 awaitAll(read, write)
             }
         }
