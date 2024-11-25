d24dbba4516d519c2b39a612b30d42d006a351db
benkuly <kontakt@folivo.net>
2024-11-25 14:49:10 +0100

    add docs

--- trixnity-client/src/commonMain/kotlin/net/folivo/trixnity/client/media/PlatformMedia.kt
+++ trixnity-client/src/commonMain/kotlin/net/folivo/trixnity/client/media/PlatformMedia.kt
@@ -2,6 +2,14 @@ package net.folivo.trixnity.client.media
 
 import net.folivo.trixnity.utils.ByteArrayFlow
 
+/**
+ * Depending on the configured [MediaStore] you may have access to more than the [ByteArrayFlow]. For example:
+ *
+ * ```kotlin
+ * check(platformMedia is OpfsPlatformMedia)
+ * platformMedia.getTemporaryFile()
+ * ```
+ */
 interface PlatformMedia : ByteArrayFlow {
     fun transformByteArrayFlow(transformer: (ByteArrayFlow) -> ByteArrayFlow): PlatformMedia
 }
