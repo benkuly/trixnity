f42da973f4dbf4b86e6426759ed1456b299e3391
benkuly <kontakt@folivo.net>
2025-01-24 14:34:14 +0100

    Rollback of cache on failed transactions additionally to database rollback.

--- trixnity-client/src/commonMain/kotlin/net/folivo/trixnity/client/store/TransactionManager.kt
+++ trixnity-client/src/commonMain/kotlin/net/folivo/trixnity/client/store/TransactionManager.kt
@@ -1,17 +1,22 @@
 package net.folivo.trixnity.client.store
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.withContext
 import net.folivo.trixnity.client.store.repository.RepositoryTransactionManager
 import net.folivo.trixnity.utils.concurrentMutableList
 import kotlin.coroutines.CoroutineContext
+
+private val log = KotlinLogging.logger {}
 
 interface TransactionManager {
     suspend fun transaction(block: suspend CoroutineScope.() -> Unit)
 }
 
 class Transaction : CoroutineContext.Element {
-    val doAfter = concurrentMutableList<suspend () -> Unit>()
+    val onCommitActions = concurrentMutableList<suspend () -> Unit>()
+    val onRollbackActions = concurrentMutableList<suspend () -> Unit>()
 
     override val key: CoroutineContext.Key<*> = Key
 
@@ -22,11 +27,26 @@ class TransactionManagerImpl(private val repositoryTransactionManager: Repositor
     TransactionManager {
     override suspend fun transaction(block: suspend CoroutineScope.() -> Unit) {
         val transaction = Transaction()
-        repositoryTransactionManager.writeTransaction {
-            withContext(KeyStore.SkipOutdatedKeys + transaction) {
-                block()
+        try {
+            repositoryTransactionManager.writeTransaction {
+                withContext(KeyStore.SkipOutdatedKeys + transaction) {
+                    block()
+                }
             }
+            val onCommitActions = transaction.onCommitActions.read { toList() }
+            if (onCommitActions.isNotEmpty()) {
+                log.trace { "apply commit actions for transaction" }
+                onCommitActions.forEach { it() }
+            }
+        } catch (exception: Exception) {
+            withContext(NonCancellable) {
+                val onRollbackActions = transaction.onRollbackActions.read { reversed() }
+                if (onRollbackActions.isNotEmpty()) {
+                    log.debug { "apply rollback actions for transaction" }
+                    onRollbackActions.forEach { it() }
+                }
+            }
+            throw exception
         }
-        transaction.doAfter.read { toList() }.forEach { it() }
     }
 }
