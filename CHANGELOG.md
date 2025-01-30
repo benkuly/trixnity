313f919f5f21c407fdbcb1bcfaaa4dac26c66f45
benkuly <kontakt@folivo.net>
2025-01-30 20:14:26 +0100

    Fixed use authentication with `GetPublicRooms`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 
 - Fixed use authentication with `GetMediaConfigLegacy`
+- Fixed use authentication with `GetPublicRooms`
 
 ### Security
 
--- trixnity-clientserverapi/trixnity-clientserverapi-model/src/commonMain/kotlin/net/folivo/trixnity/clientserverapi/model/rooms/GetPublicRooms.kt
+++ trixnity-clientserverapi/trixnity-clientserverapi-model/src/commonMain/kotlin/net/folivo/trixnity/clientserverapi/model/rooms/GetPublicRooms.kt
@@ -3,8 +3,6 @@ package net.folivo.trixnity.clientserverapi.model.rooms
 import io.ktor.resources.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import net.folivo.trixnity.core.Auth
-import net.folivo.trixnity.core.AuthRequired
 import net.folivo.trixnity.core.HttpMethod
 import net.folivo.trixnity.core.HttpMethodType.GET
 import net.folivo.trixnity.core.MatrixEndpoint
@@ -15,7 +13,6 @@ import net.folivo.trixnity.core.MatrixEndpoint
 @Serializable
 @Resource("/_matrix/client/v3/publicRooms")
 @HttpMethod(GET)
-@Auth(AuthRequired.NO)
 data class GetPublicRooms(
     @SerialName("limit") val limit: Long? = null,
     @SerialName("server") val server: String? = null,
