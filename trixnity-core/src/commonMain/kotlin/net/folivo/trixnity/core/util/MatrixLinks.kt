0740b314e2a88fcba9f269ceca918c4edf41cd5b
Janne Mareike Koschinski <mail@justjanne.de>
2025-07-21 10:26:51 +0200

    Improve error logging

--- trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/util/MatrixLinks.kt
+++ trixnity-core/src/commonMain/kotlin/net/folivo/trixnity/core/util/MatrixLinks.kt
@@ -1,11 +1,14 @@
 package net.folivo.trixnity.core.util
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.http.*
 import net.folivo.trixnity.core.model.EventId
 import net.folivo.trixnity.core.model.Mention
 import net.folivo.trixnity.core.model.RoomAliasId
 import net.folivo.trixnity.core.model.RoomId
 import net.folivo.trixnity.core.model.UserId
+
+private val log = KotlinLogging.logger {}
 
 object MatrixLinks {
     private val matrixProtocol = URLProtocol("matrix", 0)
@@ -28,12 +31,18 @@ object MatrixLinks {
     private fun parseMatrixTo(path: List<String>, parameters: Parameters): Mention? {
         val parts = path.map { id ->
             when {
-                id.length > 255 -> null
+                id.length > 255 -> {
+                    log.trace { "malformed matrix link: id too long: ${id.length} (max length: 255)" }
+                    null
+                }
                 id.startsWith(RoomAliasId.sigilCharacter) -> RoomAliasId(id)
                 id.startsWith(RoomId.sigilCharacter) -> RoomId(id)
                 id.startsWith(UserId.sigilCharacter) -> UserId(id)
                 id.startsWith(EventId.sigilCharacter) -> EventId(id)
-                else -> null
+                else -> {
+                    log.trace { "malformed matrix link: invalid id type: ${id.firstOrNull()} (known types: #, !, @, $)" }
+                    null
+                }
             }
         }
         val first = parts.getOrNull(0)
@@ -44,19 +53,28 @@ object MatrixLinks {
             first is EventId -> Mention.Event(null, first, parameters)
             first is RoomId && second is EventId -> Mention.Event(first, second, parameters)
             first is RoomId -> Mention.Room(first, parameters)
-            else -> null
+            else -> {
+                log.trace { "malformed matrix link: unknown format" }
+                null
+            }
         }
     }
 
     private fun parseMatrixProtocol(path: List<String>, parameters: Parameters): Mention? {
         val parts = path.windowed(2, 2).map { (type, id) ->
             when {
-                id.length > 255 -> null
+                id.length > 255 -> {
+                    log.trace { "malformed matrix link: id too long: ${id.length} (max length: 255)" }
+                    null
+                }
                 type == "roomid" -> RoomId("!$id")
                 type == "r" -> RoomAliasId("#$id")
                 type == "u" -> UserId("@$id")
                 type == "e" -> EventId("$$id")
-                else -> null
+                else -> {
+                    log.trace { "malformed matrix link: invalid id type: $type (known types: roomid, r, u, e)" }
+                    null
+                }
             }
         }
         val first = parts.getOrNull(0)
@@ -66,7 +84,10 @@ object MatrixLinks {
             first is RoomAliasId -> Mention.RoomAlias(first, parameters)
             first is RoomId && second is EventId -> Mention.Event(first, second, parameters)
             first is RoomId -> Mention.Room(first, parameters)
-            else -> null
+            else -> {
+                log.trace { "malformed matrix link: unknown format" }
+                null
+            }
         }
     }
 }
