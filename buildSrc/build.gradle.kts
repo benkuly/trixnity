0dd5136999fbae7b53b0a22839bea326e85a90d2
Felix Hilgers <contact@fhilgers.com>
2026-01-21 17:37:47 +0100

    ceil jvm version to build buildSrc to 24 because 25 is unsupported

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,12 @@
+import org.gradle.internal.jvm.Jvm
+
 plugins {
     `kotlin-dsl`
 }
+
+// TODO: update this when kotlin supports Java 25
+val javaVersion = minOf(Jvm.current().javaVersion?.ordinal ?: 24, 24)
+kotlin.jvmToolchain(javaVersion)
 
 dependencies {
     implementation(sharedLibs.plugins.kotlin.multiplatform.asLibrary())
